7eb06e61580b8456f0171956c9fac2405aab162f
dmusser14 <dmusser28@gmail.com>
2024-02-27 19:55:56 -0500

    3.6 React Bootstrap

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 export const LoginView = ({ onLoggedIn }) => {
   const [username, setUsername] = useState("");
@@ -37,25 +39,32 @@ export const LoginView = ({ onLoggedIn }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit}> 
-      <label>
-        Username:
-        <input 
+    <Form onSubmit={handleSubmit}> 
+      <Form.Group className="mb-3" controlId="formUsername">
+        <Form.Label>Username:</Form.Label>
+        <Form.Control
           type="text"
           value={username}
           onChange={(e) => setUsername(e.target.value)}
-          required 
+          required
+          minLength="3"
+          placeholder="Enter a username"
         />
-      </label>
-      <label>
-        Password:
-        <input 
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlID="formPassword">
+        <Form.Label>Password:</Form.Label>
+        <Form.Control 
           type="password"
           value={password}
-          onChange={(e) => setPassword(e.target.value)} />
+          onChange={(e) => setPassword(e.target.value)}
           required
-      </label>
-      <button type="submit">Submit</button>
-    </form>
+          placeholder="Enter a password"
+        /> 
+      </Form.Group>
+      <Button className="mb-3" variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
   );
 };
--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,17 +1,22 @@
 // Here you import the PropTypes library
 import PropTypes from "prop-types";
+import { Button, Card } from "react-bootstrap";
 
 // The MovieCard function component
 export const MovieCard = ({ movie, onMovieClick }) => {
   return (
-    <div
-      onClick={() => {
-        onMovieClick(movie);
-      }}
-    >
-      {movie.title}
-    </div>
-  );
+    // making whole card clickable
+    <Card className="h-100" onClick={() => onMovieClick(movie)}>
+      <Card.Img variant="top" src={movie.image} />
+      <Card.Body>
+        <Card.Title>{movie.title}</Card.Title>
+        <Card.Text>{movie.director.name}</Card.Text>
+        <Button onClick={() => onMovieClick(movie)} variant="link">
+          Open
+        </Button>
+      </Card.Body>
+    </Card>
+  );  
 };
 
 // Here is where we define all the props constraints for the MovieCard
